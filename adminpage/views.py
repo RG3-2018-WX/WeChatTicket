40a0b1061f59161b713641b08067b947868cd3f0
panhanlin16 <40332634+panhanlin16@users.noreply.github.com>
2018-10-20 10:50:48 +0800

    Update views.py

--- adminpage/views.py
+++ adminpage/views.py
@@ -6,16 +6,18 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import logout
 from wechat.models import Activity,Ticket
 global user_status
-user_status=0
+user_status = 0
 class Login(APIView):
-    global user_status
+
     def get(self):
+        global user_status
         if user_status==1:
             return 0
         else:
             return 1
 
     def post(self):
+        global user_status
         self.check_input('username', 'password')
         user = authenticate(username=self.input['username'], password=self.input['password'])
         if user is not None:
@@ -31,8 +33,9 @@ class Login(APIView):
 
 
 class Logout(APIView):
-    global user_status
+   
     def post(self):
+        global user_status
         if user_status==1:
             logout(self)
             user_status=0
